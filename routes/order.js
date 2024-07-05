90b6149e62a9dda21c63712d0e4a7e6d7e09bfaf
Muhammad Alyan <alyan0332@gmail.com>
2024-07-05 08:24:47 +0500

    Add order routes

---
+++ routes/order.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const verifyJwtToken = require('../middleware/verifyJwtToken');
+
+// const getAllOrders = require('../controllers/order/getAllOrders');
+// const getOrder = require('../controllers/order/getOrder');
+// const deleteOrder = require('../controllers/order/deleteOrder');
+const createOrder = require('../controllers/order/createOrder');
+// const updateOrder = require('../controllers/order/updateOrder');
+
+// router.get('/', getAllOrders);
+
+// router.get('/:orderId', getOrder);
+
+router.post('/', verifyJwtToken, createOrder);
+// router.put('/:orderId', verifyJwtToken, updateOrder);
+// router.post('/order/:orderId', verifyJwtToken, orderController);
+// router.delete('/:orderId', verifyJwtToken, deleteOrder);
+
+module.exports = router;
