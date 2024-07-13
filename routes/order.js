20f575f90bfca26f02f3c824d2a049eeeb4b83e5
Muhammad Alyan <alyan0332@gmail.com>
2024-07-13 23:19:24 +0500

    Add view order route

--- routes/order.js
+++ routes/order.js
@@ -7,12 +7,14 @@ const getAllOrders = require('../controllers/order/getAllOrders');
 // const deleteOrder = require('../controllers/order/deleteOrder');
 const createOrder = require('../controllers/order/createOrder');
 const updateOrder = require('../controllers/order/updateOrder');
+const addReview = require('../controllers/order/addReview');
 router.get('/', getAllOrders);
 
 // router.get('/:orderId', getOrder);
 
 router.post('/', verifyJwtToken, createOrder);
 router.put('/:orderId', verifyJwtToken, updateOrder);
+router.put('/add-review/:orderId', verifyJwtToken, addReview);
 // router.post('/order/:orderId', verifyJwtToken, orderController);
 // router.delete('/:orderId', verifyJwtToken, deleteOrder);
 
