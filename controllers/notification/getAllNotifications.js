857b8c59a0508d44b5939902ed4b6d1724bf009a
Muhammad Alyan <alyan0332@gmail.com>
2024-07-14 17:07:41 +0500

    Create route to retrieve notifications

---
+++ controllers/notification/getAllNotifications.js
@@ -0,0 +1,29 @@
+const Notification = require('../../db/model/notification');
+const mongoose = require('mongoose');
+
+const getAllNotifications = async (req, res) => {
+  const user = req.user;
+  const profileId = user?.profileId;
+
+  const { getAllNotifications } = req.query;
+  const read = [false];
+
+  if (getAllNotifications) {
+    read.push(true);
+  }
+
+  const allNotifications = await Notification.find({
+    receiverId: new mongoose.Types.ObjectId(profileId),
+    isRead: { $in: read }
+  })
+    .populate({ path: 'senderId', model: 'Profile' })
+    .populate({ path: 'receiverId', model: 'Profile' });
+
+  return res.status(200).json({
+    success: true,
+    data: allNotifications,
+    message: 'Successfully retrieved all notifications.'
+  });
+};
+
+module.exports = getAllNotifications;
