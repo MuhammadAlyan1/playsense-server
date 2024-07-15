aea5c762b73d8279eb03c32ad77c88463d8d60a4
Muhammad Alyan <alyan0332@gmail.com>
2024-07-15 14:01:06 +0500

    Update routes

--- routes/profile.js
+++ routes/profile.js
@@ -5,14 +5,15 @@ const verifyJwtToken = require('../middleware/verifyJwtToken');
 const getProfile = require('../controllers/profile/getProfile');
 const createProfile = require('../controllers/profile/createProfile');
 const updateProfile = require('../controllers/profile/updateProfile');
+const assignRoles = require('../controllers/profile/assignRoles');
 const manageFriends = require('../controllers/profile/manageFriends');
 
 router.get('/:profileId', getProfile);
-
 router.post('/', verifyJwtToken, createProfile);
 router.post('/friend', verifyJwtToken, manageFriends);
 
 router.put('/', verifyJwtToken, updateProfile);
+router.put('/assign-roles', verifyJwtToken, assignRoles);
 
 // router.post('/feedback/:profileId', verifyJwtToken, profileController);
 
