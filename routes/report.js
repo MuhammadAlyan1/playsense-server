33cb52c36c954cfeac5155bf935d8ce8c807edae
Muhammad Alyan <alyan0332@gmail.com>
2024-07-15 21:19:53 +0500

    Add route to get all reports

--- routes/report.js
+++ routes/report.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const createReport = require('../controllers/report/createReport');
+const getAllReports = require('../controllers/report/getAllReports');
 const verifyJwtToken = require('../middleware/verifyJwtToken');
 
+router.get('/', verifyJwtToken, getAllReports);
 router.post('/', verifyJwtToken, createReport);
 
 module.exports = router;
