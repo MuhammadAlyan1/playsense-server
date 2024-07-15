f5b8871f6630af7f80a4686e0f82246cdbc61096
Muhammad Alyan <alyan0332@gmail.com>
2024-07-16 00:49:55 +0500

    Add route to update feedback status

--- routes/feedback.js
+++ routes/feedback.js
@@ -6,6 +6,7 @@ const getAllFeedback = require('../controllers/feedback/getAllFeedback');
 const getFeedback = require('../controllers/feedback/getFeedback');
 const deleteFeedback = require('../controllers/feedback/deleteFeedback');
 const createFeedback = require('../controllers/feedback/createFeedback');
+const updateStatus = require('../controllers/feedback/updateStatus');
 const feedbackController = require('../controllers/feedback/feedbackController');
 
 router.get('/', getAllFeedback);
@@ -14,7 +15,7 @@ router.get('/:feedbackId', getFeedback);
 
 router.post('/', verifyJwtToken, createFeedback);
 router.post('/feedback/:feedbackId', verifyJwtToken, feedbackController);
-
+router.put('/:feedbackId', verifyJwtToken, updateStatus);
 router.delete('/:feedbackId', verifyJwtToken, deleteFeedback);
 
 module.exports = router;
