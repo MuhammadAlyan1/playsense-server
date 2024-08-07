b5f982f34a9b452797cdb031b922d369e02ff17f
Muhammad Alyan <alyan0332@gmail.com>
2024-08-07 23:24:07 +0500

    Add route for adding legend

--- routes/gameDatabase.js
+++ routes/gameDatabase.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const verifyJwtToken = require('../middleware/verifyJwtToken');
 
 const addWeapon = require('../controllers/gameDatabase/addWeapon');
+const addLegend = require('../controllers/gameDatabase/addLegend');
 
 router.post('/weapon', verifyJwtToken, addWeapon);
+router.post('/legend', verifyJwtToken, addLegend);
 
 module.exports = router;
