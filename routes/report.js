3e1f8bc824f87d12c6872eeab1bf3bb168b260e9
Muhammad Alyan <alyan0332@gmail.com>
2024-08-08 00:22:54 +0500

    Add route to delete reported content

--- routes/report.js
+++ routes/report.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const createReport = require('../controllers/report/createReport');
 const getAllReports = require('../controllers/report/getAllReports');
 const verifyJwtToken = require('../middleware/verifyJwtToken');
-
+const removeReportedItem = require('../controllers/report/removeReportedItem');
 router.get('/', verifyJwtToken, getAllReports);
 router.post('/', verifyJwtToken, createReport);
+router.delete('/delete-content/:reportId', verifyJwtToken, removeReportedItem);
 
 module.exports = router;
